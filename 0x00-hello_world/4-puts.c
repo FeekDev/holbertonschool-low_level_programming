83dadf32e194ec53d5e2cfd13cd4e411d806ba2f
felipeholbi <4758@holbertonschool.com>
2022-02-10 20:43:57 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point, prints Hello, World
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
