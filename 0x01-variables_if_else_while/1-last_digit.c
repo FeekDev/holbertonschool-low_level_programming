01141de654a0ad9b7e7db1d7e99070b967a05e84
felipeholbi <4758@holbertonschool.com>
2022-02-14 12:48:32 -0800

    last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-printf("Last digit of %i is %i and is", n, (n % 10));
+printf("Last digit of %i is %i and is ", n, (n % 10));
 if ((n % 10) > 5)
 {
 printf("greater than 5\n");
@@ -30,5 +30,5 @@ else
 {
 printf("less than 6 and not 0\n");
 }
- return (0);
+return (0);
 }
