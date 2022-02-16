582ee4febf63ac2f2a4748bd65c35bd76c569982
felipeholbi <4758@holbertonschool.com>
2022-02-16 05:23:00 -0800

    functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,21 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ *_putchar is a fubctin print ever word c, in a string
+ *
+ *
+ */
+
 int _putchar(char c);
+
+
+/**
+ *print_alphabet - print lowercase alphabet
+ *
+ *
+ */
+
+void print_alphabet(void);
+
+#endif /* _MAIN_H_ */
