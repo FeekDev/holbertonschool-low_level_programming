aa1147d2ad858500266f2f8f4b5eec3a02ec9dcd
felipeholbi <4758@holbertonschool.com>
2022-02-17 15:03:33 -0800

    largest_number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,17 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+/**
+ *
+ * posotive or negative 
+ **/
 void positive_or_negative(int i);
+
+/**
+ *
+ *most largest number
+ **/
+
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
