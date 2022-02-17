aaab5ffa6b52b259cdbd5c92692f6ae8805a6135
felipeholbi <4758@holbertonschool.com>
2022-02-17 12:06:04 -0800

    time_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table -  that prints the table time 9
+ *
+ *Return: void
+ **/
+
+void times_table(void)
+{
+
+int col = 0, row, result;
+
+for (row = 0; row <= 9; row++)
+{
+	for (col = 0; col <= 9; col++)
+	{
+	result = (row * col);
+
+	if (result == 0)
+	{
+	_putchar('0' + result);
+	}
+
+	else if (result <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + (result));
+	}
+
+	else if (result > 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar('0' + (result / 10));
+		_putchar('0' + (result % 10));
+	}
+}
+_putchar('\n');
+}
+}
