e73eb0f2094b658c30ccdbd58763d404483d50b6
felipeholbi <4758@holbertonschool.com>
2022-02-21 21:40:52 -0800

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,8 +11,19 @@
  */
 void print_rev(char *s)
 {
+	int len = 0;
+	int i;
+	/* this block war the string */
 	while (*s != '\0')
 	{
-	*s++;
+		len++;
+		++s;
 	}
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(*s);
+			s--;
+	}
+	_putchar('\n');
 }
