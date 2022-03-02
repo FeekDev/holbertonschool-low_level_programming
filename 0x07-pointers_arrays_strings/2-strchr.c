7eb19915ef0a618faa12c1db26173e24651f873c
felipeholbi <4758@holbertonschool.com>
2022-03-01 20:06:15 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,37 +3,19 @@
 #include <string.h>
 
 /**
- * _strlen - this function used from library
- *
- *@s: the character send with parameter
- *
- * Return: Always 0.
- **/
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		++len;
-		++s;
-	}
-	return (len);
-}
-
-/**
  * _strchr - find a char
  * @s: is a array
  * @c: is a char wanted
- * Return: Always 0.
+ * Return: null.
  **/
 
 char *_strchr(char *s, char c)
 {
-int i;
+int i = 0;
 
-for (i = 0; i < _strlen(s); i++)
+while (s[i] != '\0')
 {
+	i++;
 	if (s[i] == c)
 	{
 	return (s + i);
