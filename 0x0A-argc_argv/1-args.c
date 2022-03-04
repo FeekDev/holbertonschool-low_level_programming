84001599e920429cbc140affa341136878ad7102
felipeholbi <4758@holbertonschool.com>
2022-03-04 11:31:18 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - this is the topic main
+ * @argc: the  counter
+ * @argv: the array
+ * Return: this return 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
