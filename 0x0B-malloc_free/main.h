07423915cfda97a4f2381570fc2df08f9ca15f4d
felipeholbi <4758@holbertonschool.com>
2022-03-07 21:49:13 -0800

    files

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,15 @@
+/**
+ *
+ *this function is same of putchar
+ */
+
+int _putchar(char c);
+
+/**
+ *
+ *that creates an array of chars, and initializes it with a specific char.
+ **/
+
+char *create_array(unsigned int size, char c);
+
+
