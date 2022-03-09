eaab19206f8c54477398613609ab74ef179810a4
felipeholbi <4758@holbertonschool.com>
2022-03-09 07:59:38 -0800

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - the function concatena two string in using malloc
+ * @s1: the parameter string destiny
+ * @s2: the parameter string source
+ * Return: Return the concat
+ **/
+
+char *str_concat(char *s1, char *s2)
+{
+	int d = 0, s = 0;
+	int i = 0, f = 0;
+	int back = 0;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (*(s1 + d))/* Calculate size of array*/
+		i++, d++;
+
+	while (*(s2 + s))
+		f++, s++;
+	s++;/*Add the null*/
+
+	concat = malloc((s + d) *  sizeof(char));
+
+f = back;
+i = back;
+
+	while (i < d)
+	{
+		*(concat + i) = *(s1 + i);
+		i++;
+	}
+
+	while (f < s)
+	{
+		*(concat + i) = *(s2 + f);
+		i++, f++;
+	}
+
+return (concat);
+}
