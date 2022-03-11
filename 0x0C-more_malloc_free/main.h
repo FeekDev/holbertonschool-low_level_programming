d102f285d40d78ae839dd0bc64ea5a0beb7a523b
felipeholbi <4758@holbertonschool.com>
2022-03-11 08:08:43 -0800

    main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,24 @@
+/**
+ *
+ *  this function that allocates memory using malloc
+ *
+ **/
+
+void *malloc_checked(unsigned int b);
+
+/**
+ *
+ * this function that concatenates two strings
+ *
+ **/
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/**
+ *
+ * this function coun the size of array
+ *
+ **/
+
+int _strlen(char *s);
