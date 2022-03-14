55083be3eb84167f9e9b5e8ed7eee189b49d0128
felipeholbi <4758@holbertonschool.com>
2022-03-14 13:23:25 -0700

    2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - this is a function void
+ * Return: void
+ **/
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__); /* _Base_FILE macro obtained the path*/
+	return (0);
+}
