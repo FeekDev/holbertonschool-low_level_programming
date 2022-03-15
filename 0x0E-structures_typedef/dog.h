f68ed8d6ae46721b5dbaafd048dbd9125070f84a
felipeholbi <4758@holbertonschool.com>
2022-03-14 21:46:58 -0700

    init

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdio.h>
-
-/**
- *my_dog - typedef for struct dog
- **/
-
-typedef struct dog my_dog;
 
 /**
  * struct dog - this is the function
@@ -21,12 +14,6 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- *
- *function that initialize a variable
- *
- **/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
