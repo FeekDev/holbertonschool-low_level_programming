a79a995f3cc6c38859caa62710947da204cb8bd2
felipeholbi <4758@holbertonschool.com>
2022-03-17 19:56:08 -0700

    0_print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "function_pointers.h"
 
 /**
  * print_name - this is the function pointers
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-/**
- * this function that prints a name
- *
- **/
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
 
 void print_name(char *name, void (*f)(char *));
+
+#endif
