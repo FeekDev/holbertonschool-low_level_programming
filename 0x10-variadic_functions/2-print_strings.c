f24772ab1827d4adafcff04b73a004c7e2b75911
felipeholbi <4758@holbertonschool.com>
2022-03-18 21:25:10 -0700

    print_strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - this function print number
+ * @separator: this parameter check the comas
+ * @n: the count of number
+ * Return: void
+ **/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list str;
+	char *s;
+
+	if (n > 0)
+	{
+		va_start(str, n);
+
+		for (i = 1; i <= n; i++)
+		{
+			s = va_arg(str, char *);
+			if (s == NULL)
+				printf("nil");
+			else
+				printf("%s", s);
+
+			if (i != n && separator != NULL)
+				printf("%s", separator);
+		}
+		va_end(str);
+	}
+	printf("\n");
+}
