a539d1bd8911f4ced96027473df557548100bb90
felipeholbi <4758@holbertonschool.com>
2022-03-30 12:06:48 -0500

    elements&traversal

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,5 +2,23 @@
 
 size_t print_list(const list_t *h)
 {
+    unsigned int count = 0;
 
+    list_t *ptr = NULL;
+
+    ptr = h;
+
+    if (ptr->str == NULL)
+    {
+        printf("[0] (nil)");
+    }
+
+    while (ptr != NULL)
+    {
+        printf("[%d] %s\n",ptr->len, ptr->str);
+        count ++;
+        ptr = ptr->next;
+    }
+
+    return count;
 }
