05120aeab1b5a309188fa09c2d68b08c9670eedb
felipeholbi <4758@holbertonschool.com>
2022-04-01 01:21:20 -0500

    add_node_front

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - function add at begining the node
+ *
+ * @head: the pointer null
+ * @n: the dta type int
+ * Return: new nodo
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t)); /*create node*/
+
+	new_node->n = n; /*asign the date*/
+	new_node->next = *head; /*asign th adress*/
+
+	*head = new_node; /*change the head*/
+
+	return (new_node);
+}
