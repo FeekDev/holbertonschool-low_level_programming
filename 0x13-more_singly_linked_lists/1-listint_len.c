b7ce247e9f746ae1972eafe51f8c817a5adf5749
felipeholbi <4758@holbertonschool.com>
2022-04-01 12:22:40 -0500

    lenght

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,20 @@
 #include "lists.h"
 
+/**
+ * listint_len - function pint the lenght
+ *
+ * @h: this param is the arguement
+ * Return: size_t:
+ */
+
 size_t listint_len(const listint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-  while (h != NULL)
-  {
-    h = h->next;
-    count++;
-  }
-  return (count);
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
