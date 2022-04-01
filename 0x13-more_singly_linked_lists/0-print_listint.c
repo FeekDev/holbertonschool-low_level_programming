0f2e423efdcdcb5c5aa56f04b96bdffccbd2bd89
felipeholbi <4758@holbertonschool.com>
2022-03-31 23:58:19 -0500

    elements_of_node

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - function print all elements of node
+ *
+ * @h: param th pointer
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->n != NULL)
+			printf("%d\n", h->n);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
