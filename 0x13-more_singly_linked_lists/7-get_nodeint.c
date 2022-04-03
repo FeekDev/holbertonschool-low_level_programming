a892860ae5ed49b4c71726343e9a63695f0263d8
felipeholbi <4758@holbertonschool.com>
2022-04-02 18:31:26 -0700

    index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - the parameter find a node
+ * @head: the param pointer
+ * @index: the param send index
+ * Return: return a pointer
+ **/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	unsigned int count = 0;
+
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+		count++;
+		current = current->next;
+	}
+	return (NULL);
+}
