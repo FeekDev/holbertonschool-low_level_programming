d0ec1702a5337df2ffa0d4c1d7778c3309261498
felipeholbi <4758@holbertonschool.com>
2022-04-06 11:14:20 -0500

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,8 +21,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY, 0400);
 
-	/*if (fd == -1)
-		return (0);*/
+	if (fd == -1)
+		return (0);
 
 	out = read(fd, buf, letters);
 	write(STDOUT_FILENO, buf, out);
