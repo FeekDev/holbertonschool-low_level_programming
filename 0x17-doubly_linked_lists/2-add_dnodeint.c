0a788e1586f26fe16a4d1174690b9ff04094f15c
felipeholbi <4758@holbertonschool.com>
2022-05-12 08:14:16 -0500

    fix_2&lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,9 +11,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	/* allocat memory and structure*/
-	dlistint_t *new_node = new_node;
-
-	new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node = (dlistint_t)malloc(sizeof(dlistint_t));
 	/* in case of fail*/
 	if (new_node == NULL)
 		return (NULL);
