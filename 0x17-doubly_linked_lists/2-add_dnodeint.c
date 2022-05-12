de66942d6f0126ee9eaeb4c9128d86044cc03e38
felipeholbi <4758@holbertonschool.com>
2022-05-12 08:05:03 -0500

    2_fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,21 +1,33 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint - function
+ *
+ * @head: double pinter
+ * @n: data
+ * Return: address new node
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    /* allocat memory and structure*/
-    dlistint_t *new_node = new_node;
-    new_node = malloc(sizeof(dlistint_t));
-    /* assing data to newNode*/
-    new_node->n = n;
-    /*point next of newNode to the first node of double linked list*/
-    new_node->next = (*head);
-    /* point prev to null*/
-    new_node->prev = NULL;
-    /* point previous of the first node (now first node is second node)*/
-    if ((*head) != NULL)
-    new_node->prev = new_node;
-    /* head points to newNode*/
-    (*head) = new_node;
+	/* allocat memory and structure*/
+	dlistint_t *new_node = new_node;
 
-    return 0;
+	new_node = malloc(sizeof(dlistint_t));
+	/* in case of fail*/
+	if (new_node == NULL)
+		return (NULL);
+	/* assing data to newNode*/
+	new_node->n = n;
+	/*point next of newNode to the first node of double linked list*/
+	new_node->next = (*head);
+	/* point prev to null*/
+	new_node->prev = NULL;
+	/* point previous of the first node (now first node is second node)*/
+	if ((*head) != NULL)
+		new_node->prev = new_node;
+	/* head points to newNode*/
+	(*head) = new_node;
+
+return (new_node);
 }
