bf931b4e17f350ac3965f48c65dcc7b25ee3c1cb
felipeholbi <4758@holbertonschool.com>
2022-05-12 08:50:38 -0500

    fix_2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,22 +10,22 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	/* allocat memory and structure*/
-	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	/* allocate the memory and struct*/
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	/* in case of fail*/
 	if (new_node == NULL)
+	{
 		return (NULL);
-	/* assing data to newNode*/
+	}
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	/* allocat the data*/
 	new_node->n = n;
-	/*point next of newNode to the first node of double linked list*/
-	new_node->next = (*head);
-	/* point prev to null*/
-	new_node->prev = NULL;
-	/* point previous of the first node (now first node is second node)*/
-	if ((*head) != NULL)
-		new_node->prev = new_node;
-	/* head points to newNode*/
-	(*head) = new_node;
+	/* set the head */
+	new_node->next = *head;
+	*head = new_node;
 
 return (new_node);
 }
