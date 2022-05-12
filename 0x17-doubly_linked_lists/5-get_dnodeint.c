c398a9f914da2da37400b010047c6d146c7bcab6
felipeholbi <4758@holbertonschool.com>
2022-05-12 12:29:13 -0500

    5.3_fix

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,24 +11,21 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	/* allow memory and variable*/
-	dlistint_t *temp;
 	unsigned int i = 0;
 	/* in case node doesn't exist */
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	/* point in head*/
-	temp = (head);
 	/* traversal and conditional*/
-	while (i < index)
+	while (i != index)
 	{
 		if (head == NULL)
 		{
 			return (NULL);
 		}
-		temp = temp->next;
+		head = head->next;
 		i++;
 	}
-	return (temp);
+	return (head);
 }
