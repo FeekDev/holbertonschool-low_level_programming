7565f67bfa06c465e774b2582ac5cba1b08f418e
felipeholbi <4758@holbertonschool.com>
2022-06-06 12:01:35 -0500

    0_hash&_README

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - the function creates a hash table
+ * @size: passed argument size
+ * Return: the pointer to newly created hash table
+ **/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ha_table = NULL;
+	unsigned long int i;
+    /*Allocate the table itself.*/
+	ha_table = malloc(sizeof(hash_table_t));
+	if (!ha_table)
+		return (NULL);
+    /* Allocate pointers to the head nodes. */
+	ha_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ha_table->array)
+	{
+		free(ha_table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		ha_table->array[i] = NULL;
+	}
+
+	ha_table->size = size;
+	return (ha_table);
+}
