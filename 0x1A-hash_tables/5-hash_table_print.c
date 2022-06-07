8b15103db396b633a5f0be552db51e662b4ea0f0
felipeholbi <4758@holbertonschool.com>
2022-06-06 22:01:20 -0500

    5_hash_&_header

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print -  that prints a hash table
+ *
+ * @ht: is the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node = 0;
+	int print_separator = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (print_separator)
+			{
+				printf(", ");
+			}
+			printf("'%s': '%s'", node->key, node->value);
+			print_separator = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
