04bbd09fb3b2ca8f2c9774b828419bc28d6fb8db
felipeholbi <4758@holbertonschool.com>
2022-06-06 21:40:34 -0500

    4_hash_&_header

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ *
+ * @ht: is the hash table you want to look into
+ * @key: the value of string
+ * Return: char*
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *pair;
+
+	index = key_index((unsigned char *)key, ht->size);
+
+    /* Step through the bin, looking for our value. */
+	pair = ht->array[index];
+	while (pair != NULL && pair->key != NULL && strcmp(key, pair->key) > 0)
+	{
+		pair = pair->next;
+	}
+
+    /* Did we actually find anything? */
+	if (pair == NULL || pair->key == NULL || strcmp(key, pair->key) != 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (pair->value);
+	}
+}
